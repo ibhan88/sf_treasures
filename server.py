d6c1200f73f6d482f4c6255d061792ead69b23f4
Lora Rose <aroseartist@gmail.com>
2016-09-10 13:31:47 -0700

    Added imports

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 """SF Treasures - The All-Women Hackathon San Francisco 9/10/16"""
 
 from flask import Flask, render_template, request, flash, redirect, session, jsonify
-# import geocoder
-# import geojson
-# import googlemaps
+import geocoder
+import geojson
+import googlemaps
+from Jinja2 import StrictUndefined
 import json
 import os
 import requests
@@ -13,9 +14,13 @@ import sys
 app = Flask(__name__)
 
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+# Access key for Googlemaps API
+gmaps = googlemaps.Client(key=os.environ['GEOLOCATE_GOOGLE_API'])
 
-# app.jinja_env.undefined = StrictUndefined
-
+# Ensures undefined variables in jinja raise an error
+app.jinja_env.undefined = StrictUndefined
+# allows html to reload without restarting server
+app.jinja_env.auto_reload = True
 
 ###############################################################################
 
@@ -43,6 +48,7 @@ def show_winner():
 
 ##########################################################################
 
+# Listening or requests
 if __name__ == "__main__":
     # connect_to_db(app)
     app.run(host="0.0.0.0")
