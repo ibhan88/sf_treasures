b5626cea093192a54a7b721715e1f7a200bb6eaf
lindsaynchan <lindsaynchan@gmail.com>
2016-09-10 10:49:41 -0700

    Set up server.py

--- server.py
+++ server.py
@@ -1,3 +1,27 @@
-hello world!
+"""SF Treasures - The All-Women Hackathon San Francisco 9/10/16"""
 
-lalalallaa
+import os
+from flask import Flask, render_template, request, flash, redirect, session, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+
+app.jinja_env.undefined = StrictUndefined
+
+
+###############################################################################
+
+
+@app.route('/')
+def show_index():
+
+
+##########################################################################
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0")
+    
